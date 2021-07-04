869701219c8fb7dc24e93f18dcb650c34710d38b
mukhammadsaid19 <m.mamasaidov@student.inha.uz>
2021-07-04 21:08:14 +0500

    first try

--- foma/flookup.c
+++ foma/flookup.c
@@ -58,15 +58,13 @@ static char *helpstring =
 
 struct lookup_chain {
     struct fsm *net;
-    struct apply_handle *ah;
-	struct apply_med_handle *medh;
+    struct apply_med_handle *ah;
     struct lookup_chain *next;
     struct lookup_chain *prev;
 };
 
 #define DIR_DOWN 0
 #define DIR_UP 1
-#define MED 2
 
 static struct sockaddr_in serveraddr, clientaddr;
 static int                listen_sd, numbytes;
@@ -79,7 +77,7 @@ static FILE *INFILE;
 static struct lookup_chain *chain_head, *chain_tail, *chain_new, *chain_pos;
 static fsm_read_binary_handle fsrh;
 
-static char *(*applyer)() = &apply_up;  /* Default apply direction = up */
+static char *(*applyer)() = &apply_med;  /* Default apply direction = up */
 static void handle_line(char *s);
 static void app_print(char *result);
 static char *get_next_line();
@@ -122,126 +120,126 @@ int main(int argc, char *argv[]) {
 
     setvbuf(stdout, buffer, _IOFBF, sizeof(buffer));
 
-  while ((opt = getopt(argc, argv, "mabhHiI:qs:SA:P:w:vx")) != -1) {
-    switch(opt) {
-        case 'a':
-	    	apply_alternates = 1;
-	    	break;
-        case 'b':
-	    	buffered_output = 0;
-	    	break;
-        case 'h':
-	    	printf("%s%s\n", usagestring,helpstring);
-            exit(0);
-        case 'i':
-	    	direction = DIR_DOWN;
-	    	applyer = &apply_down;
-	    	break;
-		case 'm':
-			direction = MED;
-			applyer = &apply_med;
-			break;
-        case 'q':
-	    	sortarcs = 0;
-	    	break;
-		case 'I':
-			if (strcmp(optarg, "f") == 0) {
-			index_flag_states = 1;
-			index_arcs = 1;
-			} else if (strstr(optarg, "k") != NULL && strstr(optarg,"K") != NULL) {
-			/* k limit */
-			index_mem_limit = 1024*atoi(optarg);
-			index_arcs = 1;
-			} else if (strstr(optarg, "m") != NULL && strstr(optarg,"M") != NULL) {
-			/* m limit */
-			index_mem_limit = 1024*1024*atoi(optarg);
-			index_arcs = 1;
-			} else if (isdigit(*optarg)) {
-			index_arcs = 1;
-			index_cutoff = atoi(optarg);
-			}
-			break;
-		case 's':
-			separator = strdup(optarg);
-			break;
-		case 'S':
-			mode_server = 1;
-			break;
-		case 'A':
-			server_address = strdup(optarg);
-			break;
-		case 'P':
-			port_number = atoi(optarg);
-			break;
-		case 'w':
-			wordseparator = strdup(optarg);
-			break;
-			case 'v':
-			printf("flookup 1.03 suka (foma library version %s)\n", fsm_get_library_version_string());
-			exit(0);
-			case 'x':
-			echo = 0;
-			break;
-		default:
-			fprintf(stderr, "%s", usagestring);
-			exit(EXIT_FAILURE);
+  while ((opt = getopt(argc, argv, "abhHimI:qs:SA:P:w:vx")) != -1) {
+   switch(opt) {
+	case 'a':
+		apply_alternates = 1;
+		break;
+	case 'b':
+		buffered_output = 0;
+		break;
+	case 'h':
+	printf("%s%s\n", usagestring,helpstring);
+		exit(0);
+	case 'i':
+		direction = DIR_DOWN;
+		applyer = &apply_down;
+		break;
+	case 'q':
+		sortarcs = 0;
+		break;
+	case 'I':
+	    if (strcmp(optarg, "f") == 0) {
+		index_flag_states = 1;
+		index_arcs = 1;
+	    } else if (strstr(optarg, "k") != NULL && strstr(optarg,"K") != NULL) {
+		/* k limit */
+		index_mem_limit = 1024*atoi(optarg);
+		index_arcs = 1;
+	    } else if (strstr(optarg, "m") != NULL && strstr(optarg,"M") != NULL) {
+		/* m limit */
+		index_mem_limit = 1024*1024*atoi(optarg);
+		index_arcs = 1;
+	    } else if (isdigit(*optarg)) {
+		index_arcs = 1;
+		index_cutoff = atoi(optarg);
+	    }
+	    break;
+	case 's':
+	    separator = strdup(optarg);
+	    break;
+	case 'S':
+	    mode_server = 1;
+	    break;
+	case 'A':
+	    server_address = strdup(optarg);
+	    break;
+	case 'P':
+	    port_number = atoi(optarg);
+	    break;
+	case 'w':
+	    wordseparator = strdup(optarg);
+	    break;
+        case 'v':
+	    printf("flookup 1.03 (foma library version %s)\n", fsm_get_library_version_string());
+	    exit(0);
+        case 'x':
+	    echo = 0;
+	    break;
+	default:
+            fprintf(stderr, "%s", usagestring);
+            exit(EXIT_FAILURE);
 		}
     }
+
+
+
     if (optind == argc) {
-	fprintf(stderr, "%s", usagestring);
-	exit(EXIT_FAILURE);
+		fprintf(stderr, "%s", usagestring);
+		exit(EXIT_FAILURE);
     }
 
     infilename = argv[optind];
 
     if ((fsrh = fsm_read_binary_file_multiple_init(infilename)) == NULL) {
         perror("File error");
-	exit(EXIT_FAILURE);
+		exit(EXIT_FAILURE);
     }
+
     chain_head = chain_tail = NULL;
 
     while ((net = fsm_read_binary_file_multiple(fsrh)) != NULL) {
-	numnets++;
-	chain_new = malloc(sizeof(struct lookup_chain));
-	if (direction == DIR_DOWN && net->arcs_sorted_in != 1 && sortarcs) {
-	    fsm_sort_arcs(net, 1);
-	}
-	if (direction == DIR_UP && net->arcs_sorted_out != 1 && sortarcs) {
-	    fsm_sort_arcs(net, 2);
-	}
-	chain_new->net = net;
-	chain_new->ah = apply_init(net);
-	chain_new->medh = apply_med_init(net);
+		numnets++;
+		chain_new = malloc(sizeof(struct lookup_chain));
+		// if (direction == DIR_DOWN && net->arcs_sorted_in != 1 && sortarcs) {
+		// 	fsm_sort_arcs(net, 1);
+		// }
+		// if (direction == DIR_UP && net->arcs_sorted_out != 1 && sortarcs) {
+		// 	fsm_sort_arcs(net, 2);
+		// }
+		chain_new->net = net;
+		chain_new->ah = apply_med_init(net);
+		apply_med_set_heap_max(chain_new->ah, 4194304);    /* Don't grow heap more than this            */
+		apply_med_set_med_limit(chain_new->ah, 10);       /* Don't search for matches with cost > 10   */
+		apply_med_set_med_cutoff(chain_new->ah, 5);       /* Don't return more than 5 matches          */
 
-	if (direction == DIR_DOWN && index_arcs) {
-	    apply_index(chain_new->ah, APPLY_INDEX_INPUT, index_cutoff, index_mem_limit, index_flag_states);
-	}
-	if (direction == DIR_UP && index_arcs) {
-	    apply_index(chain_new->ah, APPLY_INDEX_OUTPUT, index_cutoff, index_mem_limit, index_flag_states);
-	}
+		// if (direction == DIR_DOWN && index_arcs) {
+		// 	apply_index(chain_new->ah, APPLY_INDEX_INPUT, index_cutoff, index_mem_limit, index_flag_states);
+		// }
+		// if (direction == DIR_UP && index_arcs) {
+		// 	apply_index(chain_new->ah, APPLY_INDEX_OUTPUT, index_cutoff, index_mem_limit, index_flag_states);
+		// }
 
-	// if (direction == MED && index_arcs) {
-	// 	apply_med(chain_new->medh, )
-	// }
 
-	chain_new->next = NULL;
-	chain_new->prev = NULL;
-	if (chain_tail == NULL) {
-	    chain_tail = chain_head = chain_new;
-	} else if (direction == DIR_DOWN || apply_alternates == 1) {
-	    chain_tail->next = chain_new;
-	    chain_new->prev = chain_tail;
-	    chain_tail = chain_new;
-	} else {
-	    chain_new->next = chain_head;
-	    chain_head->prev = chain_new;
-	    chain_head = chain_new;
-	}
+
+		chain_new->next = NULL;
+		chain_new->prev = NULL;
+		if (chain_tail == NULL) {
+			chain_tail = chain_head = chain_new;
+		} else if (direction == DIR_DOWN || apply_alternates == 1) {
+			chain_tail->next = chain_new;
+			chain_new->prev = chain_tail;
+			chain_tail = chain_new;
+		} else {
+			chain_new->next = chain_head;
+			chain_head->prev = chain_new;
+			chain_head = chain_new;
+		}
     }
 
     if (numnets < 1) {
-	fprintf(stderr, "%s: %s\n", "File error", infilename);
-	exit(EXIT_FAILURE);
+		fprintf(stderr, "%s: %s\n", "File error", infilename);
+		exit(EXIT_FAILURE);
     }
 
     if (mode_server) {
@@ -286,13 +284,14 @@ int main(int argc, char *argv[]) {
 	    if (!buffered_output) {
 		fflush(stdout);
 	    }
-	}
+	  }
     }
+	
    /* Cleanup */
     for (chain_pos = chain_head; chain_pos != NULL; chain_pos = chain_head) {
 	chain_head = chain_pos->next;
 	if (chain_pos->ah != NULL) {
-	    apply_clear(chain_pos->medh);
+	    apply_clear(chain_pos->ah);
 	}
 	if (chain_pos->net != NULL) {
 	    fsm_destroy(chain_pos->net);
@@ -337,7 +336,7 @@ void handle_line(char *s) {
 
 	/* Get result from chain */
 	for (chain_pos = chain_head, tempstr = s;  ; chain_pos = chain_pos->next) {
-	    result = applyer(chain_pos->medh, tempstr);
+	    result = applyer(chain_pos->ah, tempstr);
 	    if (result != NULL && chain_pos != chain_tail) {
 		tempstr = result;
 		continue;
@@ -346,12 +345,12 @@ void handle_line(char *s) {
 		do {
 		    results++;
 		    app_print(result);
-		} while ((result = applyer(chain_pos->medh, NULL)) != NULL);
+		} while ((result = applyer(chain_pos->ah, NULL)) != NULL);
 	    }
 	    if (result == NULL) {
 		/* Move up */
 		for (chain_pos = chain_pos->prev; chain_pos != NULL; chain_pos = chain_pos->prev) {
-		    result = applyer(chain_pos->medh, NULL);
+		    result = applyer(chain_pos->ah, NULL);
 		    if (result != NULL) {
 			tempstr = result;
 			break;
